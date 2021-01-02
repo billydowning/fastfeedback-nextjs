bf61ffeb010c7388e8478aec062f2346da613853
Billy Downing <wldowning@gmail.com>
2021-01-01 17:38:10 -0700

    changed global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,38 @@
 import Head from "next/head";
-import "@/styles/globals.css";
 import { AuthProvider } from "@/lib/auth";
-import { ChakraProvider, Global, extendTheme } from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
+import { ChakraProvider, extendTheme, CSSReset } from "@chakra-ui/react";
 import theme from "@/styles/theme";
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: "100vh";
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 const App = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
+        <GlobalStyle />
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>
