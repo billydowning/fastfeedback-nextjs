737289aca392b43786fcfe83c4644e526828a0d6
Billy Downing <wldowning@gmail.com>
2021-01-03 18:30:31 -0700

    set up SWR

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,16 @@
 import DashboardShell from "@/components/DashboardShell";
 import Overview from "@/components/Overview";
 import EmptyState from "@/components/EmptyState";
+import useSWR from "swr";
+import fetcher from "@/utils/fetcher";
 
 import { useAuth } from "@/lib/auth";
 
 const Dashboard = () => {
-  // const auth = useAuth();
+  const auth = useAuth();
+  const { data } = useSWR("/api/appts", fetcher);
+
+  console.log(data);
 
   // if (!auth.user) {
   //   return "Loading...";
